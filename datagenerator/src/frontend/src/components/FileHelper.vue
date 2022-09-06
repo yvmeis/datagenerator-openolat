8db2c1d61ce789448999c69ccf448a3144e7dedf
yvmeis <71340078+yvmeis@users.noreply.github.com>
2022-09-06 13:00:12 +0200

    helppage update

--- datagenerator/src/frontend/src/components/FileHelper.vue
+++ datagenerator/src/frontend/src/components/FileHelper.vue
@@ -43,9 +43,33 @@
       </div>
       <div style="display:flex; flex-direction: column; align-items: center;">
         <p class="tutorialtext">
-          The <span class="highlighted">courseZips</span> folder must contain all .zip exported Courses that were mentioned in the courses.csv<br>
-          Make sure the names are exactly the same!
+          The <span class="highlighted">curricula</span> folder can contain arbitrarily many curricula. They must all be named: "curriculum{number}"<br>
+          Each curriculum folder must contain a <span class="highlighted">coursecurriculumrelations.csv</span>, <span class="highlighted">curriculumname.csv</span>, <span class="highlighted">levels.csv</span> and <span class="highlighted">types.csv</span>.
         </p>
+        <img style="height: 16vh; width: 24vh;" src = "../assets/curriculum-structure.jpg"/>
+        <p class="tutorialtext">
+          In the <span class="highlighted">1st column</span> of the coursecurriculumrelations.csv file you must put the name of Curriculum Element.<br>
+          In the <span class="highlighted">2nd - nth column</span> you must put the name of the Course you wish to add to the Element.
+        </p>
+        <label>coursecurriculumrelations.csv</label>
+        <img style="height: 28vh; width: 69vh;" src = "../assets/curriculumrelations.jpg"/>
+        <p class="tutorialtext">
+          In the <span class="highlighted">1st column</span> of the curriculumname.csv file you must put the name of Curriculum.<br>
+          In the <span class="highlighted">2nd column</span> you must put the identifier of the Curriculum.
+        </p>
+        <label>curriculumname.csv</label>
+        <img style="height: 9vh; width: 30vh;" src = "../assets/curriculumnames.jpg"/>
+        <p class="tutorialtext">
+          -----------------------------------------------------------
+        </p>
+        <label>levels.csv</label>
+        <img style="height: 8vh; width: 45vh;" src = "../assets/groups.jpg"/>
+        <p class="tutorialtext">
+          In the <span class="highlighted">1st column</span> of the types.csv file you must put the name of type.<br>
+          In the <span class="highlighted">2nd column</span> you must put the identifier of the type.
+        </p>
+        <label>types.csv</label>
+        <img style="height: 16vh; width: 40vh;" src = "../assets/curriculumtypes.jpg"/>
       </div>
       <div class="tutorialtitle">
         Groups
@@ -66,9 +90,33 @@
       </div>
       <div style="display:flex; flex-direction: column; align-items: center;">
         <p class="tutorialtext">
-          The <span class="highlighted">courseZips</span> folder must contain all .zip exported Courses that were mentioned in the courses.csv<br>
-          Make sure the names are exactly the same!
+          The <span class="highlighted">taxonomies</span> folder can contain arbitrarily many taxonomies. They must all be named: "taxonomy{number}"<br>
+          Each taxonomy folder must contain a <span class="highlighted">coursetaxonomyrelations.csv</span>, <span class="highlighted">taxonomyname.csv</span>, <span class="highlighted">levels.csv</span> and <span class="highlighted">types.csv</span>.
         </p>
+        <img style="height: 16vh; width: 24vh;" src = "../assets/taxonomy-structure.jpg"/>
+        <p class="tutorialtext">
+          In the <span class="highlighted">1st column</span> of the coursetaxonomyrelations.csv file you must put the name of Taxonomy Level.<br>
+          In the <span class="highlighted">2nd - nth column</span> you must put the name of the Course you wish to add to the Level.
+        </p>
+        <label>coursetaxonomyrelations.csv</label>
+        <img style="height: 26vh; width: 77vh;" src = "../assets/taxonomyrelations.jpg"/>
+        <p class="tutorialtext">
+          In the <span class="highlighted">1st column</span> of the taxonomyname.csv file you must put the name of Taxonomy.<br>
+          Remember to create the Taxonomy in the OO instance!
+        </p>
+        <label>taxonomyname.csv</label>
+        <img style="height: 9vh; width: 20vh;" src = "../assets/taxonomyname.jpg"/>
+        <p class="tutorialtext">
+          -----------------------------------------------------------
+        </p>
+        <label>levels.csv</label>
+        <img style="height: 16vh; width: 60vh;" src = "../assets/groups.jpg"/>
+        <p class="tutorialtext">
+          In the <span class="highlighted">1st column</span> of the types.csv file you must put the name of type.<br>
+          In the <span class="highlighted">2nd column</span> you must put the identifier of the type.
+        </p>
+        <label>types.csv</label>
+        <img style="height: 16vh; width: 32vh;" src = "../assets/taxonomytypes.jpg"/>
       </div>
       <div class="tutorialtitle">
         Users
@@ -82,7 +130,7 @@
           In the <span class="highlighted">3rd column</span> is the firstname of the User.<br>
           In the <span class="highlighted">4th column</span> is the gender of the User. This is solely for the purposes of picking a profilepicture<br>
           From the <span class="highlighted">5th column onward</span> you can define where the User is put.<br>
-          Always start off with a identifier in <span class="highlighted">all Caps</span>. They are COURSE,CURRICULUM,GROUP and ROLES.<br>
+          Always start off with an identifier in <span class="highlighted">all Caps</span>. They are COURSE, CURRICULUM, GROUP and ROLES.<br>
           After <span class="highlighted">COURSE</span> you must add the Course name and then the desired roles: participant, tutor or owner.<br>
           After <span class="highlighted">GROUP</span> you must add the Group name and then the desired roles: participant or owner.<br>
           After <span class="highlighted">CURRICULUM</span> you must add the Curriculum name and then <span class="highlighted">ELEMENT</span><br>
@@ -100,7 +148,7 @@
           "curriculumManager"<br>
         </p>
         <label>users.csv</label>
-        <img style="height: 16vh; width: 90vh;" src = "../assets/users.jpg"/>
+        <img style="height: 15vh; width: 90vh;" src = "../assets/users.jpg"/>
       </div>
   </div>
 </template>
